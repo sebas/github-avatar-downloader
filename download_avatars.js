c932d79d271d2a35c46af1905aa2f6f835c126c6
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-09-03 23:35:00 +0000

    Now with authorization and token, kept secret.

--- download_avatars.js
+++ download_avatars.js
@@ -7,9 +7,22 @@ function getRepoContributors(repoOwner, repoName, cb) {
     console.log("Great so you want:");
     console.log("Repository owner", repoOwner);
     console.log("And the repositoru:", repoName);
+
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+          'User-Agent': 'request',
+          'Authorization': ''
+        }
+      };
+
+    request(options, function(err, res, body) {
+        cb(err, body);
+    });  
   }
 
-  getRepoContributors("jquery", "jquery", function(err, result) {
+  // getRepoContributors("jquery", "jquery", function(err, result) {
+  getRepoContributors("sebas", "nodefsex", function(err, result) {
     console.log("Errors:", err);
     console.log("Result:", result);
   }); 
