a8ff2419132e08f3e84a5d3f352a246fd5f3b2d7
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-09-03 23:19:10 +0000

    We are now calling a function to get the requested meta information printed out.

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,15 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+    // ...
+    console.log("Great so you want:");
+    console.log("Repository owner", repoOwner);
+    console.log("And the repositoru:", repoName);
+  }
+
+  getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+  }); 
