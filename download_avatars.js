7199ced756d1254d9cbcc6bd49214d5938ce0516
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-09-04 00:59:35 +0000

    Some more code clean up.

--- download_avatars.js
+++ download_avatars.js
@@ -9,58 +9,54 @@ var secrets = require('./secrets');
 let repoOwner = process.argv[2];
 let repoName = process.argv[3];
 
-if(repoOwner === undefined || repoName === undefined) {
-    console.log("You need to pass both parameters");
-} else {
+// Given an URL and a path save that to our
+// hard drive.
+function downloadImageByURL(url, filePath) {
 
-console.log("repoOwner: ", repoOwner);
-console.log("repoName: ", repoName);
-
-console.log('Welcome to the GitHub Avatar Downloader!');
+  request.get(url)
+    .on('error', function (err) {
+      throw err; 
+    })
+    .pipe(fs.createWriteStream(filePath)
+    );     
+}
 
 // Get a repository owner and a repository and get all the
 // contributors information from Github.
 // Pass that information to our callback function
 function getRepoContributors(repoOwner, repoName, cb) {
-    console.log("Great so you want:");
-    console.log("   Repository owner: ", repoOwner);
-    console.log("   And the repository: ", repoName);
+  console.log("Great so you want:");
+  console.log("   Repository owner: ", repoOwner);
+  console.log("   And the repository: ", repoName);
 
-    var options = {
-        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-        headers: {
-          'User-Agent': 'request',
-          'Authorization': 'token ' + secrets.GITHUB_TOKEN
-        }
-      };
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secrets.GITHUB_TOKEN
+    }
+  };
 
-    request(options, function(err, res, body) {
-        cb(err, JSON.parse(body));
-    });  
-  }
+  request(options, function(err, res, body) {
+    cb(err, JSON.parse(body));
+  });  
+}
 
-    // Loop through the avatars calling a function to download them.
-    getRepoContributors(repoOwner, repoName, function(err, result) {     
+if(repoOwner === undefined || repoName === undefined) {
+  console.log("You need to pass both parameters");
+} else {
+
+  console.log("repoOwner: ", repoOwner);
+  console.log("repoName: ", repoName);
+
+  console.log('Welcome to the GitHub Avatar Downloader!');
+
+  // Loop through the avatars calling a function to download them.
+  getRepoContributors(repoOwner, repoName, function(err, result) {     
     for (let index = 0; index < result.length; index++) {
-        const element = result[index];
-        downloadImageByURL( element.avatar_url, 'avatars/' + index + '.jpg');
+      const element = result[index];
+      downloadImageByURL( element.avatar_url, 'avatars/' + index + '.jpg');
     }
     console.log('Download complete.');
   }); 
-
-  // Given an URL and a path save that to our
-  // hard drive.
-  function downloadImageByURL(url, filePath) {
-
-    request.get(url)
-        .on('error', function (err) {
-            throw err; 
-        })
-        .on('end', function (response) {
-            
-        })
-        .pipe(fs.createWriteStream(filePath)
-        );     
-  }
-
 }
