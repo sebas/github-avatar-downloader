5ec6aec352046d78988f8bb082f0284cdc6718d3
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-09-04 00:25:04 +0000

    We have now connected the getRepo wih the download.

--- download_avatars.js
+++ download_avatars.js
@@ -1,28 +1,59 @@
 var request = require('request');
+var fs = require('fs');
+var secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    // ...
     console.log("Great so you want:");
-    console.log("Repository owner", repoOwner);
-    console.log("And the repositoru:", repoName);
+    console.log("   Repository owner: ", repoOwner);
+    console.log("   And the repository: ", repoName);
 
     var options = {
         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
         headers: {
           'User-Agent': 'request',
-          'Authorization': ''
+          'Authorization': 'token ' + secrets.GITHUB_TOKEN
         }
       };
 
+    // console.log("opt ", options);
+
     request(options, function(err, res, body) {
-        cb(err, body);
+        console.log("body: ", body);      
+        console.log("err: ", err);
+        cb(err, JSON.parse(body));
     });  
   }
 
-  // getRepoContributors("jquery", "jquery", function(err, result) {
-  getRepoContributors("sebas", "nodefsex", function(err, result) {
+  getRepoContributors("jquery", "jquery", function(err, result) {
+  // getRepoContributors("sebas", "nodefsex", function(err, result) {
     console.log("Errors:", err);
     console.log("Result:", result);
+    for (let index = 0; index < result.length; index++) {
+        const element = result[index];
+        console.log("The avatar URL", element.avatar_url);
+        downloadImageByURL( element.avatar_url, 'avatars/' + index + '.jpg');
+    }
   }); 
+
+  function downloadImageByURL(url, filePath) {
+    console.log('Downloading image...');
+
+    request.get(url)
+        .on('error', function (err) {
+            throw err; 
+        })
+        .on('response', function (response) {
+            console.log('Response Status Code: ', response.statusCode);
+        })
+        .on('end', function (response) {
+            console.log('Download complete.');
+        })
+        .pipe(fs.createWriteStream(filePath)
+        );     
+  }
+
+  // https://avatars1.githubusercontent.com/u/43004?v=4
+
+downloadImageByURL("https://avatars1.githubusercontent.com/u/43004?v=4", "avatars/kvirani.jpg");
